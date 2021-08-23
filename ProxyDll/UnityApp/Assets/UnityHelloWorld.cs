7c4b7b6215cd87f649faa8c7f17b7d22497fe421
UpHash <yukawallstudio@gmail.com>
2021-08-24 00:07:42 +0900

    ProxyDllの寄生先UnityPJ追加

---
+++ ProxyDll/UnityApp/Assets/UnityHelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class UnityHelloWorld : MonoBehaviour
+{
+    public string Message = "UnityHelloWorld! ハローゆにてい。";
+
+    void OnGUI()
+    {
+       GUI.Label(new Rect(10, 10, 500, 20), Message);
+    }
+}
